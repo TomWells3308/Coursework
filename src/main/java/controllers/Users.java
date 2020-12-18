396fb91be64e6bc3479aa132690bafded8e10f4d
TomWells3308 <71508223+TomWells3308@users.noreply.github.com>
2020-12-18 17:51:56 +0000

    Update Users.java

--- src/main/java/controllers/Users.java
+++ src/main/java/controllers/Users.java
@@ -15,6 +15,13 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLOutput;
 
+import javax.ws.rs.CookieParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.UUID;
+
 @Path("user/")
 @Consumes(MediaType.MULTIPART_FORM_DATA)
 @Produces(MediaType.APPLICATION_JSON)
@@ -79,6 +86,38 @@ public class Users {
         catch (Exception exception){
             System.out.println("Database error: " + exception.getMessage());
             return "{\"Error\": \"Unable to delete item, please see server console for more info.\"}";
+        }
+    }
+
+    @POST
+    @Path("login")
+    public String loginUser(@FormDataParam("Username") String Username, @FormDataParam("Password") String Password){
+        System.out.println("Invoked loginUser() on path user/login");
+        try{
+            PreparedStatement ps1 = Main.db.prepareStatement("SELECT Password FROM Users WHERE Username = ?");
+            ps1.setString(1, Username);
+            ResultSet loginResults = ps1.executeQuery();
+            if (loginResults.next() == true) {
+                String correctPassword = loginResults.getString(1);
+                if (Password.equals(correctPassword)) {
+                    String token = UUID.randomUUID().toString();
+                    PreparedStatement ps2 = Main.db.prepareStatement("UPDATE Users SET Token = ? WHERE Username = ?");
+                    ps2.setString(1, token);
+                    ps2.setString(2, Username);
+                    ps2.executeUpdate();
+                    JSONObject userDetails = new JSONObject();
+                    userDetails.put("username", Username);
+                    userDetails.put("token", token);
+                    return userDetails.toString();
+                } else {
+                    return "{\"Error\": \"Incorrect password!\"}";
+                }
+            } else {
+                return "{\"Error\": \"Username and password are incorrect.\"}";
+            }
+        } catch (Exception exception) {
+            System.out.println("Database error during /user/login: " + exception.getMessage());
+            return "{\"Error\": \"Server side error!\"}";
         }
     }
 }
