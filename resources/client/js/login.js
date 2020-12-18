245309365faf4346b573687abfb317cf53478499
TomWells3308 <71508223+TomWells3308@users.noreply.github.com>
2020-12-18 17:49:56 +0000

    Update login.js

--- resources/client/js/login.js
+++ resources/client/js/login.js
@@ -76,3 +76,26 @@ function redraw(){
     }
     window.requestAnimationFrame(redraw);
 }
+
+function postUserLogin(){
+    console.log("Invoked postUserLogin() ");
+
+    let url = "/user/login";
+    let formData = new FormData(document.getElementById('loginForm'));
+
+    fetch(url, {
+        method: "POST",
+        body: formData,
+    }).then(response => {
+        return response.json();                 //now return that promise to JSON
+    }).then(response => {
+        if (response.hasOwnProperty("Error")) {
+            alert(JSON.stringify(response));        // if it does, convert JSON object to string and alert
+        } else {
+            Cookies.set("token", response.token);
+            Cookies.set("username", response.username);
+            window.open("index.html", "_self");       //open index.html in same tab
+        }
+    });
+
+}
